2d41bc1ab8d0b27a035bbb02619b1fae2cc44c70
Danna García <dannasofiagarciatrujillo@hotmail.com>
2021-02-21 12:44:37 -0500

    New p2 image

--- gmap-military/controller/MainWindow.cs
+++ gmap-military/controller/MainWindow.cs
@@ -86,6 +86,7 @@ namespace gmap_military
                 hastaTB.Enabled = false;
                 filterB.Enabled = false;
 
+
             } else if (filterOptionsCB.SelectedItem.Equals("Cadena"))
             {   //Ciudades
 
@@ -98,7 +99,6 @@ namespace gmap_military
             } else if (filterOptionsCB.SelectedItem.Equals("Numérico"))
             {
                 //Número zona
-
                 msgLabel.Text = "Escriba el rango de las zonas por las que desea filtrar los datos";
                 desdeTB.Enabled = true;
                 hastaTB.Enabled = true;
