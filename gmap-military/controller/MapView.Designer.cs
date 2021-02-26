7a8b9f66f62e435b2a9317d502beea436f640634
Camilo Cordoba <kamneklogs@gmail.com>
2021-02-26 16:46:58 -0500

    Finished mapFilter

--- gmap-military/controller/MapView.Designer.cs
+++ gmap-military/controller/MapView.Designer.cs
@@ -30,6 +30,9 @@ namespace gmap_military.controller
         private void InitializeComponent()
         {
             this.mainMap = new GMap.NET.WindowsForms.GMapControl();
+            this.filterValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainMap
@@ -42,8 +45,8 @@ namespace gmap_military.controller
             this.mainMap.GrayScaleMode = false;
             this.mainMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.mainMap.LevelsKeepInMemmory = 5;
-            this.mainMap.Location = new System.Drawing.Point(14, 4);
-            this.mainMap.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.mainMap.Location = new System.Drawing.Point(6, 4);
+            this.mainMap.Margin = new System.Windows.Forms.Padding(6);
             this.mainMap.MarkersEnabled = true;
             this.mainMap.MaxZoom = 20;
             this.mainMap.MinZoom = 2;
@@ -57,18 +60,47 @@ namespace gmap_military.controller
             this.mainMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.mainMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.mainMap.ShowTileGridLines = false;
-            this.mainMap.Size = new System.Drawing.Size(1850, 906);
+            this.mainMap.Size = new System.Drawing.Size(1262, 378);
             this.mainMap.TabIndex = 0;
             this.mainMap.Zoom = 5D;
             this.mainMap.Load += new System.EventHandler(this.gMapLoad);
+            // 
+            // filterValue
+            // 
+            this.filterValue.Location = new System.Drawing.Point(194, 512);
+            this.filterValue.Name = "filterValue";
+            this.filterValue.Size = new System.Drawing.Size(100, 20);
+            this.filterValue.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 479);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Filtrar por zona militar (1-15)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 495);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Filtrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // MapView
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1880, 1031);
+            this.ClientSize = new System.Drawing.Size(1370, 616);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filterValue);
             this.Controls.Add(this.mainMap);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "MapView";
             this.Text = "Zonas Militares";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -80,5 +112,8 @@ namespace gmap_military.controller
         #endregion
 
         private GMap.NET.WindowsForms.GMapControl mainMap;
+        private System.Windows.Forms.TextBox filterValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- gmap-military/controller/MapView.cs
+++ gmap-military/controller/MapView.cs
@@ -68,9 +68,9 @@ namespace gmap_military.controller
                 string[] loc = temp[1].Split(' ');
 
                 double lat = double.Parse(loc[0], CultureInfo.InvariantCulture); //Si te pc esta en español, usa comas con los decimales
-      
+
                 double lon = double.Parse(loc[2], CultureInfo.InvariantCulture);
-                
+
 
                 PointLatLng p = new PointLatLng(lat, lon);
                 points.Add(p);
@@ -80,6 +80,68 @@ namespace gmap_military.controller
             {
                 GMapMarker marker = new GMarkerGoogle(p, GMarkerGoogleType.red_dot);
                 markers.Markers.Add(marker); //Aqui se agrega el marcador a la capa
+            }
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            data.Clear();
+            try
+            {
+                int j = Int32.Parse(filterValue.Text);
+                if (!(j > 0 && j <= 15))
+                    throw new Exception();
+
+                foreach (DataGridViewRow item in rows)
+                {
+                    int i = Int32.Parse((string)item.Cells[0].Value);
+
+
+                    if (i == j)
+                    {
+                        data.Add((string)item.Cells[2].Value + ";" + (string)item.Cells[5].Value);
+                    }
+
+
+                }
+
+
+
+
+                markers.Markers.Clear();
+                points.Clear();
+                //Aqui carga puntos
+
+                foreach (string item in data)
+                {
+                    string[] temp = item.Split(';');
+                    string[] loc = temp[1].Split(' ');
+
+                    double lat = double.Parse(loc[0], CultureInfo.InvariantCulture); //Si te pc esta en español, usa comas con los decimales
+
+                    double lon = double.Parse(loc[2], CultureInfo.InvariantCulture);
+
+                    PointLatLng p = new PointLatLng(lat, lon);
+                    points.Add(p);
+                }
+
+                foreach (PointLatLng p in points) //P es un punto creado con latitud y longitud
+                {
+                    GMapMarker marker = new GMarkerGoogle(p, GMarkerGoogleType.purple_dot);
+                    markers.Markers.Add(marker); //Aqui se agrega el marcador a la capa
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("El valor debe ser numerico");
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Los valores ingresados se salen del rango");
+
+
             }
 
         }
